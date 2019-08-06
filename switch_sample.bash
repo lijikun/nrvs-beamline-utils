828130c219aff823c5cb57e4ae1840e774fa8048
Jikun Li <jikun.li@icloud.com>
2019-08-06 13:35:24 -0400

    Correctly removes ending slashes of $1. Other misc fixes.

--- switch_sample.bash
+++ switch_sample.bash
@@ -8,13 +8,16 @@ if [[ ! $1 ]]; then
 	exit 1
 fi
 
+# Strips trailing slashes possibly at the end of $1 due to shell autocomplete.
+target="${1%%/*}"
+
 prevFile1=$(basename $(readlink in_padd))
 prevFile2=$(basename $(readlink in_phox))
 prevString1=${prevFile1#in_padd_}
 prevString2=${prevFile2#in_phox_}
 
-if [[ -e $1 && -e $1/in_padd_$1 && -e $1/in_phox_$1 ]]; then
-	# Sample data already exist.
+if [[ -e ${target} && -e ${target}/in_padd_${target} && -e ${target}/in_phox_${target} ]]; then
+	# Sample input files already exist.
 	if [[ -e in_padd ]]; then
 		echo "Backing up in_padd to in_padd.bak"
 		cp -L in_padd in_padd.bak
@@ -26,12 +29,12 @@ if [[ -e $1 && -e $1/in_padd_$1 && -e $1/in_phox_$1 ]]; then
 else 
 	# Creates new folder. Copies existing input files into it and makes edits.
 	if [[ -e in_padd && -e in_phox ]]; then
-		mkdir -p $1
-		cp -L in_padd $1/in_padd_$1
-		cp -L in_phox $1/in_phox_$1
-		if [[ $prevString1 && $prevString2 ]]; then
-			sed -i "s:$prevString1:$1:g" $1/in_padd_$1
-			sed -i "s:$prevString2:$1:g" $1/in_phox_$1
+		mkdir -p ${target}
+		cp -L in_padd ${target}/in_padd_${target}
+		cp -L in_phox ${target}/in_phox_${target}
+		if [[ ${prevString1} && ${prevString2} ]]; then
+			sed -i "s:${prevString1}:${target}:g" ${target}/in_padd_${target}
+			sed -i "s:${prevString2}:${target}:g" ${target}/in_phox_${target}
 		fi
 	else
 		echo "Must have existing in_padd and in_phox files to begin with!"
@@ -40,26 +43,28 @@ else
 fi
 
 # Moves files belonging to previous sample.
-if [[ $prevString1 && $prevString2 ]]; then
+if [[ ${prevString1} && ${prevString2} && -d ${prevString1} && -d ${prevString2} ]]; then
     for x in ${prevString1}?*; do
         if [[ -e $x ]]; then	
             echo "Moving file ${x} to folder ${prevString1}/"
-            mv $x $prevString1/
+            mv $x ${prevString1}/
         fi
     done
     for x in ${prevString2}?*; do
         if [[ -e $x ]]; then 
             echo "Moving file ${x} to folder ${prevString2}/"
-            mv $x $prevString2/
+            mv $x ${prevString2}/
         fi
     done
+else
+    echo "Cannot find the correct directory for old files. Skipping moving them."
 fi
 
 # Creates soft links.
 echo "Creating soft links:"
 rm -f in_padd in_phox
-ln -fs $1/in_padd_$1 in_padd
-ln -fs $1/in_phox_$1 in_phox
+ln -fs ${target}/in_padd_${target} in_padd
+ln -fs ${target}/in_phox_${target} in_phox
 ls -l in_padd in_phox
 echo
 
