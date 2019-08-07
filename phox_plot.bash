94bbd1859ac692a68718078302a9453f897e33c1
Jikun Li <jikun.li@icloud.com>
2019-08-07 14:42:12 -0400

    Allow forced gaussian fit of inelastic peak with --Gau

--- phox_plot.bash
+++ phox_plot.bash
@@ -4,27 +4,41 @@
 #
 # Outputs a file named [Sample Name]_dos_int.dat and plots it using gnuplot.
 #
-# Defaults to cm^-1 as energy unit. Optionally, use --meV for meV energy unit, i.e.:
+# Will use resolution function to fit the inelastic peak, 
+# if a [Sample Name]_res_sum.dat file is found.
+# Otherwise, a Gaussian function will be used instead.
+# Adding --Gau flag will force the use of Gaussian function.
+#
+# Defaults to cm^-1 as energy unit. Optionally, use --meV for meV energy unit.
+#
 ## ./phox_plot.bash		# Generates file and plot with cm^-1 as energy unit.
-## ./phox_plot.bash --meV	# Generates file and plot with meV as energy unit.
+## ./phox_plot.bash --Gau	# Forces Gaussian fit for inelastic peak.
+## ./phox_plot.bash --meV	# With meV as energy unit.
 
 file2=$(basename $(readlink in_phox))
 string2=${file2#in_phox_}
 
-if [[ $1 == '--meV' ]]; then
-	factor=1.0;
-else
-	factor=8.06554
-fi
-
-if [[ -e "${string2}_res_sum.dat" ]]; then
-	resString="${string2}_res_sum.dat 1 2 process"
-else
-	if [[ -e "${string2}/${string2}_res_sum.dat" ]]; then
-		resString="${string2}/${string2}_res_sum.dat 1 2 process"
-	else
-		resString="2"
-	fi
+factor=8.06554
+resString=
+for i in $@; do
+    if [[ $i == '--meV' ]]; then
+        factor=1.0;
+    fi
+    if [[ $i == '--Gau' ]]; then
+        resString=2;
+    fi
+done
+        
+if [[ ! $resString ]]; then 
+    if [[ -e "${string2}_res_sum.dat" ]]; then
+        resString="${string2}_res_sum.dat 1 2 process"
+    else
+        if [[ -e "${string2}/${string2}_res_sum.dat" ]]; then
+            resString="${string2}/${string2}_res_sum.dat 1 2 process"
+        else
+            resString="2"
+        fi
+    fi
 fi
 sed -i --follow-symlinks -e 's|^[[:space:]]*(3.8).*| (3.8) shape, 2=Gaussian :: '"${resString}"'|g' in_phox
 phox --nographics
