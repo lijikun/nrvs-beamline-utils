ab5019d2b757574241eee9670b5dfb997cdaecf8
Jikun Li <jikun.li@icloud.com>
2019-08-09 18:46:18 -0400

    Starting v1.1 branch with major changes

--- switch_sample.bash
+++ switch_sample.bash
@@ -27,27 +27,35 @@ prevFile2=$(basename $(readlink in_phox))
 prevString1=${prevFile1#in_padd_}
 prevString2=${prevFile2#in_phox_}
 
+# Backs up old input files if they already exist.
+if [[ -e in_padd ]]; then
+    echo "Backing up in_padd to in_padd.bak"
+    cp -L in_padd in_padd.bak
+fi
+if [[ -e in_phox ]]; then
+    echo "Backing up in_phox to in_phox.bak"
+    cp -L in_phox in_phox.bak
+fi
+
+# Creates new input files.
 if [[ -e ${target} && -e ${target}/in_padd_${target} && -e ${target}/in_phox_${target} ]]; then
-	# Sample input files already exist.
-	if [[ -e in_padd ]]; then
-		echo "Backing up in_padd to in_padd.bak"
-		cp -L in_padd in_padd.bak
-	fi
-	if [[ -e in_phox ]]; then
-		echo "Backing up in_phox to in_phox.bak"
-		cp -L in_phox in_phox.bak
-	fi
+    echo "Target directory already has input files. Will be using them."
 else 
 	# Creates new folder. Copies existing input files into it and makes edits.
 	if [[ -e in_padd && -e in_phox ]]; then
+        echo "New sample name. Will create new input files by editing existing templates."
 		mkdir -p ${target}
-		cp -L in_padd ${target}/in_padd_${target}
-		cp -L in_phox ${target}/in_phox_${target}
 		if [[ ${prevString1} && ${prevString2} ]]; then
-			sed -i "s:${prevString1}:${target}:g" ${target}/in_padd_${target}
-			sed -i "s:${prevString2}:${target}:g" ${target}/in_phox_${target}
+            # Can read sample name data from symlinks. Uses simple global search-and-replace.
+			sed -e "s:${prevString1}:${target}:g" in_padd > ${target}/in_padd_${target}
+			sed -e "s:${prevString2}:${target}:g" in_phox > ${target}/in_phox_${target}
+        else
+            # Cannot read existing sample name data. Try to edit in_padd and in_phox by section numbers instead.
+            awk 'BEGIN {FS="::"} /^[^\*]*\(8\.?\).*::/ {print $1 ":: '${target}' ($3? " ::" $3: "" ) ; next} {print}' in_padd > ${target}/in_padd_${target}
+            awk 'BEGIN {FS="::"} /^[^\*]*\(3.2\.?\).*::/ {print $1 ":: '"${target}_sum.dat 1 2"'" ($3? " ::" $3 : ""); next} /^[^\*]*\(3.4\.?\).*::/ {print $1 ":: '"${target} p 1 2 3 d n r s"'" ($3? " ::" $3 : ""); next} /^[^\*]*\(3.8\.?\).*::/ {print $1 ":: 2" ($3? " ::" $3 : ""); next} {print}' in_phox > ${target}/in_phox_${target}
 		fi
 	else
+        # No existing input files. Cannot create new ones w/o them.
 		echo "Must have existing in_padd and in_phox files to begin with!"
 		exit 1
 	fi
